1172db5f8288caee5a16f8a3e16cea38be7754a4
Frank Natividad <franknatividad@google.com>
2017-08-25 17:22:14 -0700

    Template for 3.0.6 because it hasn't been released

---
+++ appengine/refinerycms/3.0.6-template.rb
@@ -0,0 +1,49 @@
+require 'rbconfig'
+VERSION_BAND = '3.0'
+MINOR_VERSION_BAND = '3.0.0'
+
+# We want to ensure that you have an ExecJS runtime available!
+begin
+  require 'execjs'
+rescue LoadError
+  abort <<-ERROR
+\033[31m[ABORTING]\033[0m ExecJS is not installed. Please re-start the installer after running:\ngem install execjs
+ERROR
+end
+
+if File.read("#{destination_root}/Gemfile") !~ /assets.+coffee-rails/m
+  gem "coffee-rails", :group => :assets
+end
+
+append_file 'Gemfile', <<-GEMFILE
+
+# Refinery CMS
+gem 'refinerycms', git: 'https://github.com/refinery/refinerycms', branch: '3-0-stable'
+
+# Optionally, specify additional Refinery CMS Extensions here:
+gem 'refinerycms-acts-as-indexed', ['~> 2.0', '>= 2.0.1']
+gem 'refinerycms-wymeditor', ['~> 1.0', '>= 1.0.6']
+gem 'refinerycms-authentication-devise', '~> 1.0'
+#  gem 'refinerycms-blog', ['~> #{VERSION_BAND}', '>= #{MINOR_VERSION_BAND}']
+#  gem 'refinerycms-inquiries', ['~> #{VERSION_BAND}', '>= #{MINOR_VERSION_BAND}']
+#  gem 'refinerycms-search', ['~> #{VERSION_BAND}', '>= #{MINOR_VERSION_BAND}']
+#  gem 'refinerycms-page-images', ['~> #{VERSION_BAND}', '>= #{MINOR_VERSION_BAND}']
+GEMFILE
+
+begin
+  require 'execjs'
+  ::ExecJS::Runtimes.autodetect
+rescue
+  gsub_file 'Gemfile', "# gem 'therubyracer'", "gem 'therubyracer'"
+end
+
+run 'bundle install'
+
+rake 'db:create'
+generate "refinery:cms --fresh-installation #{ARGV.join(' ')}"
+
+say <<-SAY
+  ============================================================================
+    Your new Refinery CMS application is now installed and mounts at '/'
+  ============================================================================
+SAY
